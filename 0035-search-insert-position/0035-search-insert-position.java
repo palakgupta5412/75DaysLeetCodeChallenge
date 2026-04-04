f5819f31fb722cf09fa5ea3cc4de6ee65b5fe581
Palak Gupta <156589496+palakgupta5412@users.noreply.github.com>
2026-04-04 11:13:20 +0530

    Time: 0 ms (100%), Space: 44.9 MB (19.53%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -1,40 +1,9 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
-
-        // //edge cases :
-        // if(nums[0]>target){
-        //     return 0 ;
-        // }
-        // if(nums[nums.length-1]<target){
-        //     return nums.length ;
-        // }
-
-        // //Iteration :
-        // int sI=0;
-        // int eI = nums.length;
-        // while(sI<=eI && sI<nums.length && eI>=0){
-        //     int mid = (sI+eI)/2 ;
-        //     if(nums[mid]==target){
-        //         return mid ;
-        //     }
-        //     if(mid!=nums.length-1 && nums[mid]<target && nums[mid+1]>target){
-        //         return mid+1 ;
-        //     }
-        //     if(mid!=0 && nums[mid-1]<target && nums[mid]>target){
-        //         return mid ;
-        //     }
-        //     if(nums[mid]>target){
-        //         sI=mid+1;
-        //     }
-        //     else{
-        //         eI=mid-1;
-        //     }
-        // }
-        // return (sI+eI)/2 ;
-        int ans=Arrays.binarySearch(nums,target);
-        if(ans<0){
-            return -ans-1;
+        int idx=Arrays.binarySearch(nums,target);
+        if(idx<0){
+            return -idx-1;
         }
-        return ans;
+        return idx;
     }
 }
